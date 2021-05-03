1ea5311aaef96116fdc71c88b370b5a9b0805ecc
Olivia Lewis <ollybaby12@gmail.com>
2021-05-02 23:41:00 -0400

    init format

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends Component {
+  render() {
+
+    return (
+      <div className="App">
+        <nav className="App__nav">
+
+        </nav>
+        <main className="App__main">
+
+        </main>
+        <footer className="App__footer">
+
+        </footer>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
