7de8dee82abaf2efcb2ad61878b91b2e8cb94b1a
Olivia Lewis <ollybaby12@gmail.com>
2021-05-17 22:57:23 -0400

    deploy

--- src/config.js
+++ src/config.js
@@ -2,6 +2,5 @@ module.exports = {
     API_PROFILE_TYPES_URL: `https://mysterious-ocean-47868.herokuapp.com/api/profileTypes`,
     API_REGIONS_URL: `https://mysterious-ocean-47868.herokuapp.com/api/regions`,
     API_PROFILES_URL: `https://mysterious-ocean-47868.herokuapp.com/api/profiles`,
-    // API_KEY: process.env.REACT_APP_API_KEY
     API_KEY: '7f58d47c-9a92-490f-93bc-3a5abbf14cae'
 }
