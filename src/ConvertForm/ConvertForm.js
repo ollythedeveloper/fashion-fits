357c2ab5ff8040190edead2a2af14be74cfe9bb6
Olivia Lewis <ollybaby12@gmail.com>
2021-05-24 16:42:41 -0400

    style convert form

--- src/ConvertForm/ConvertForm.js
+++ src/ConvertForm/ConvertForm.js
@@ -27,8 +27,8 @@ export default function ConvertForm() {
     );
 
     const showSizeType = () => {
-        if(selectedRadio === 'letter') {
-            return letterOpts 
+        if (selectedRadio === 'letter') {
+            return letterOpts
         } else {
             return numberOpts
         }
@@ -57,29 +57,39 @@ export default function ConvertForm() {
             <section className="Conversion">
                 <form className="Convert-form">
                     <div className="Size-select">
-                        <input
-                            type="radio"
-                            name="size-type"
-                            id="size-type"
-                            value="letter"
-                            defaultChecked
-                            onChange={handleChangeRadio} />Letter
-                        <input
-                            type="radio"
-                            name="size-type"
-                            id="size-type"
-                            value="number"
-                            onChange={handleChangeRadio} />Number
+
+                        <div className="radio_container">
+                            <h2>Size:</h2>
+                            <input
+                                type="radio"
+                                className="toggle_option"
+                                name="size-type"
+                                id="letter-toggle"
+                                value="letter"
+                                defaultChecked
+                                onChange={handleChangeRadio} />
+                            <label htmlFor="letter-toggle">Letter</label>
+
+                            <input
+                                type="radio"
+                                className="toggle_option"
+                                name="size-type"
+                                id="number-toggle"
+                                value="number"
+                                onChange={handleChangeRadio} />
+                            <label htmlFor="number-toggle">Number</label>
+                        </div>
+
                         {' '}
 
-                        <select onChange={handleChangeSize}>
+                        <select id="size-scroll" size="4" onChange={handleChangeSize}>
                             {sizeOpts}
                         </select>
                     </div>
                     <div className="Convert-select">
-                        <p>Convert to: </p>
-                        <label htmlFor="convert_region">Region: </label>
-                        <select name="convert_region" id="convert_region" onChange={handleConvertRegion}>
+                        <h2>Convert Region: </h2>
+                        {/* <label htmlFor="convert_region">Region: </label> */}
+                        <select name="convert_region" id="convert_region" size="4" onChange={handleConvertRegion}>
                             {regionOpts}
                         </select>
                     </div>
