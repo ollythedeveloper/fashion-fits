c4ffa0dbf3f947716415713d56fda731960a14f5
Olivia Lewis <ollybaby12@gmail.com>
2021-06-02 21:02:08 -0400

    complete landing page

--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import huh from "./confused2.png";
-import travel from "./travel1.png";
+import travel from "./travel2.png";
 import hooray from "./happyShop.png";
 import "./Landing.css";
 
@@ -25,27 +24,32 @@ export default function Landing() {
             </header>
             <section className="Confusion">
                 <div className="group">
-                    {/* <div className="item">
-                        <img src={travel} alt="Passport" />
-                    </div> */}
-                    <div className="item">
-                        <img src={huh} alt="Confused Face" />
-                    </div>
                     <div className="item">
                         <header>
                             <h2>Skip the Confusion!</h2>
                         </header>
                         <p>Shopping for clothes online or in another country can be very difficult!</p>
+                        <p>It's frustrating to purchase clothes and later find out you got the wrong size.</p>
+                        <p>It would be amazing if you could convert your clothing size to its equivalence in another country in a matter of seconds.</p>
+                        <p>That's where <b>Fashion Fits</b> comes in!</p>
                     </div>
-                    {/* <div className="item">
-                        <img src={travel} alt="Passport" />
-                    </div> */}
                 </div>
             </section>
             <section className="Solution">
+                <h2>HOW IT WORKS:</h2>
                 <div className="group">
                     <div className="item">
                         <img src={travel} alt="Passport" />
+                    </div>
+                    <div className="item">
+                        <ol>
+                            <li>Select your size profile and region.</li>
+                            <li>Once submitted, you will arrive at the Conversion Page.</li>
+                            <li>Enter the size you want to convert. (<i>You can choose between letter or number sizes.</i>)</li>
+                            <li>Select the country you want to covert your size into.</li>
+                            <li>You will get the converted number size as well as the measurements in inches.</li>
+                        </ol>
+                        <p>HAPPY SHOPPING!!!</p>
                     </div>
                     <div className="item">
                         <img src={hooray} alt="Excited Shopper" />
